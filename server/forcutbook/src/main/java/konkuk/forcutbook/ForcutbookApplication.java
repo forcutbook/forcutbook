b93ba2c7f1899728368cc75551b65d8472cf537c
곽유나 <yoona405@konkuk.ac.kr>
2024-07-15 20:46:42 +0900

    build : application.yml설정
    database, jpa설정 추가

---
+++ server/forcutbook/src/main/java/konkuk/forcutbook/ForcutbookApplication.java
@@ -0,0 +1,13 @@
+package konkuk.forcutbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForcutbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForcutbookApplication.class, args);
+	}
+
+}
---
+++ server/forcutbook/src/test/java/konkuk/forcutbook/ForcutbookApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.forcutbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForcutbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
