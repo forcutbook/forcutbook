7e11e17e2cac4ab727ac15acfaaf99446c9681f8
곽유나 <yoona405@konkuk.ac.kr>
2024-08-22 16:17:36 +0900

    refactor : response 형식 공통 BaseResponse포맷으로 변경

--- server/forcutbook/src/main/java/konkuk/forcutbook/diary/DiaryController.java
+++ server/forcutbook/src/main/java/konkuk/forcutbook/diary/DiaryController.java
@@ -2,15 +2,12 @@ package konkuk.forcutbook.diary;
 
 import konkuk.forcutbook.diary.dto.*;
 import konkuk.forcutbook.global.response.BaseResponse;
-import konkuk.global.dto.SuccessResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @Slf4j
 @RequestMapping("/users/{userId}/diaries")
