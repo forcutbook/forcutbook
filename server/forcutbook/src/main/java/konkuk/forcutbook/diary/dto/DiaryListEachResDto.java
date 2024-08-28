8a71aa05de23dd945d6bdec653f3a48a679e0a05
곽유나 <yoona405@konkuk.ac.kr>
2024-08-28 14:59:15 +0900

    feat : 내 다이어리 목록 조회에 유저의 아이디, 이미지, 닉네임 추가

--- server/forcutbook/src/main/java/konkuk/forcutbook/diary/dto/DiaryListEachResDto.java
+++ server/forcutbook/src/main/java/konkuk/forcutbook/diary/dto/DiaryListEachResDto.java
@@ -11,6 +11,9 @@ import java.time.LocalDateTime;
 @Builder(access = AccessLevel.PROTECTED)
 @Getter
 public class DiaryListEachResDto {
+    private Long userId;
+    private String userName;
+    private String userProfile;
     private Long diaryId;
     private String title;
     private String content;
@@ -18,22 +21,14 @@ public class DiaryListEachResDto {
     private LocalDateTime createdAt;
 
     @QueryProjection
-    public DiaryListEachResDto(Long diaryId, String title, String content, String imageUrl, LocalDateTime createdAt) {
+    public DiaryListEachResDto(Long userId, String userName, String userProfile, Long diaryId, String title, String content, String imageUrl, LocalDateTime createdAt) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userProfile = userProfile;
         this.diaryId = diaryId;
         this.title = title;
         this.content = content;
         this.imageUrl = imageUrl;
         this.createdAt = createdAt;
-    }
-
-    public static DiaryListEachResDto toDto(Diary diary){
-//        String titleImgUrl = diary.getDiaryImages()
-        return DiaryListEachResDto.builder()
-                .diaryId(diary.getId())
-                .title(diary.getTitle())
-                .content(diary.getContent())
-//                .imageUrl()
-                .createdAt(diary.getCreatedAt())
-                .build();
     }
 }
